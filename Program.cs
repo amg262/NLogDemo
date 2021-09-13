8ceb4625db29ac11749f3c7b2a384245418fb1a5
Andrew <andrewmgunn26@gmail.com>
2021-09-13 09:01:26 -0500

    init

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using NLog.Web;
+using System.IO;
 
 namespace NLogDemo
 {
@@ -6,7 +8,34 @@ namespace NLogDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = Directory.GetCurrentDirectory() + "\\nlog.config";
+
+            // create instance of Logger
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
+
+            // log sample messages
+            logger.Trace("Sample trace message");
+            logger.Debug("Sample debug message");
+            logger.Info("Sample informational message");
+            logger.Warn("Sample warning message");
+            logger.Error("Sample error message");
+            logger.Fatal("Sample fatal error message");
+            
+            // NLog supports structured messages
+            var fruit = new[] { "bananas", "apples", "pears" };
+            logger.Info("I like to eat {Fruit}", fruit);
+            
+            // Example of logging an exception
+            try
+            {
+                int x = 10;
+                int y = 0;
+                Console.WriteLine(x / y);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
         }
     }
 }
